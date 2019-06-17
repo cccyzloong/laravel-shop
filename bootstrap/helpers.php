6c673ea98ea28b9bd8dca7fa280bce8873bd25cc
cccyzloong <cccyzloong@163.com>
2019-06-17 18:16:02 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,6 +3,10 @@
 /**
  * 帮助函数
  */
-function test_help(){
+/* function test_help(){
     return "ok";
+} */
+
+function route_class(){
+    return str_replace('.','-',Route::currentRouteName());
 }
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title','首页')
+    
+@section('content')
+    <h1>首页</h1>
+@endsection
