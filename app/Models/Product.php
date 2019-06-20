a4f193eb25f98fa44ab80580b499b305b77b482d
cccyzloong <cccyzloong@163.com>
2019-06-20 15:30:05 +0800

    管理后台 新建-编辑商品

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,7 +14,7 @@ class Product extends Model
     protected $cats = [
         'on_sale' => 'boolean',
     ];
-    public function sku()
+    public function skus()
     {
         return $this->hasMany('App\Models\ProductSku');
     }
