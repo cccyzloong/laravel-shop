880b146ae964db0114191dc1870429e6fd9de87e
cccyzloong <cccyzloong@163.com>
2019-06-21 13:42:03 +0800

    用户界面-商品详情页

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -40,6 +40,12 @@ class ProductsController extends Controller
                 'search' => $search,
                 'order' => $order
             ]
-        ]);
+         ]);
+    }
+    public function show(Product $product,Request $request){
+        if (!$product->on_sale) {
+            throw new \Exception('商品未上架');
+        }
+        return view('products.show',['product' => $product]);
     }
 }
