bf88cbaeefaccb7b2a754c7f289a6a96c89dc1e3
cccyzloong <cccyzloong@163.com>
2019-06-28 10:26:24 +0800

    确认收货

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -7,7 +7,7 @@ use App\Models\UserAddress;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Services\OrderService;
-
+use App\Exceptions\InvalidRequestException;
 
 class OrdersController extends Controller
 {
@@ -31,5 +31,19 @@ class OrdersController extends Controller
         $user    = $request->user();
         $address = UserAddress::find($request->input('address_id'));
         return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
+    }
+    //确认收货
+    public function received(Order $order,Request $request){
+        // 校验权限
+        $this->authorize('own',$order);
+        // 判断订单的发货状态是否为已发货
+        if ($order->ship_status !== Order::SHIP_STATUS_DELIVERED) {
+            throw new InvalidRequestException('发货状态不正确');
+        }
+        // 更新发货状态为已收到
+        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
+
+        // 返回原页面
+        return $order;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,8 @@ Route::group(['middleware' => ['auth','verified']], function () {
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
     //支付回调（前端）
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+    //收货
+    Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 //服务器端回调
