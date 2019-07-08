7165b3fc0e28588b5537aa840ad7f0a9a8ed023c
cccyzloong <cccyzloong@163.com>
2019-07-08 15:33:09 +0800

    用户界面 - 使用优惠券下单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -12,6 +12,8 @@ use Carbon\Carbon;
 use App\Http\Requests\SendReviewRequest;
 use App\Events\OrderReviewed;
 use App\Http\Requests\ApplyRefundRequest;
+use App\Exceptions\CouponCodeUnavailableException;
+use App\Models\CouponCode;
 
 class OrdersController extends Controller
 {
@@ -39,7 +41,15 @@ class OrdersController extends Controller
 
         $user    = $request->user();
         $address = UserAddress::find($request->input('address_id'));
-        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
+        $coupon  = null;
+        // 如果用户提交了优惠码
+        if ($code = $request->input('coupon_code')) {
+            $coupon = CouponCode::where('code', $code)->first();
+            if (!$coupon) {
+                throw new CouponCodeUnavailableException('优惠券不存在');
+            }
+        }
+        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'), $coupon);
     }
     //确认收货
     public function received(Order $order,Request $request){
