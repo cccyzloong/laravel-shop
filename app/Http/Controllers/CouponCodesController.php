10eb0b5e1ca773eb12b77812c1700e432bc66bbf
cccyzloong <cccyzloong@163.com>
2019-07-12 18:12:45 +0800

    优化优惠券模块

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use App\Models\CouponCode;
 use Carbon\Carbon;
@@ -9,14 +10,12 @@ use Carbon\Carbon;
 class CouponCodesController extends Controller
 {
     //
-    public function show($code){
+    public function show($code, Request $request){
 
         if (!$record = CouponCode::where('code', $code)->first()) {
             throw new CouponCodeUnavailableException('优惠券不存在');
         }
-
-        $record->checkAvailable();
-
+        $record->checkAvailable($request->user());
         return $record;
     }
 }
